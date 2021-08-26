00540cbff9a160e94d09c5b83f55da6c7d8ec0a0
Francis Libs <fr.libs@gmail.com>
2021-08-26 06:55:39 +0200

    Add crud for spare parts

--- src/Controller/PartController.php
+++ src/Controller/PartController.php
@@ -3,6 +3,8 @@
 namespace App\Controller;
 
 use App\Repository\PartRepository;
+use Knp\Component\Pager\PaginatorInterface;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
@@ -12,12 +14,17 @@ class PartController extends AbstractController
     /**
      * @Route("/part", name="part")
      */
-    public function index(PartRepository $partRepository): Response
+    public function index(Request $request, PartRepository $partRepository, PaginatorInterface $paginator): Response
     {
         $parts = $partRepository->findAll();
+        $parts = $paginator->paginate(
+            $parts, /* query NOT result */
+            $request->query->getInt('page', 1), /*page number*/
+            5 /*limit per page*/
+        );
         
         return $this->render('part/index.html.twig', [
-            'parts' => '$parts',
+            'parts' => $parts,
         ]);
     }
 }
