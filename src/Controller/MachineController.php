13f36e340b87f060ad89ee810ad4ab67fca1ca44
Francis Libs <fr.libs@gmail.com>
2021-08-26 18:52:15 +0200

    Create the Machine CRUD and the machine list template

--- src/Controller/MachineController.php
+++ src/Controller/MachineController.php
@@ -5,10 +5,11 @@ namespace App\Controller;
 use App\Entity\Machine;
 use App\Form\MachineType;
 use App\Repository\MachineRepository;
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Knp\Component\Pager\PaginatorInterface;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 /**
  * @Route("/machine")
@@ -18,10 +19,18 @@ class MachineController extends AbstractController
     /**
      * @Route("/", name="machine_index", methods={"GET"})
      */
-    public function index(MachineRepository $machineRepository): Response
+    public function index(Request $request, PaginatorInterface $paginator, MachineRepository $machineRepository ): Response
     {
-        return $this->render('machine/index.html.twig', [
-            'machines' => $machineRepository->findAll(),
+        $machines = $machineRepository->findAll();
+
+        $machines = $paginator->paginate(
+            $machines, /* query NOT result */
+            $request->query->getInt('page', 1), /*page number*/
+            10 /*limit per page*/
+        );
+
+        return $this->render('machine/list.html.twig', [
+            'machines' => $machines,
         ]);
     }
 
