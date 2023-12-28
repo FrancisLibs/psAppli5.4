b8e227de052baf73f14378aeb0cc838033c54d6d
Francis Libs <fr.libs@gmail.com>
2023-12-28 07:47:23 +0100

    I change on the home page, the management of infos frame. Adding of a little javascript script the color of the border of them.

---
+++ public/assets/js/infosHomePage.js
@@ -0,0 +1,4 @@
+if (nbOfLateBT > 10 || partsToBuy > 20 || lateParts > 10) {
+  // Ajoutez la classe "flashing-border" à l'élément que vous souhaitez encadrer.
+  document.getElementById("infoFrame").classList.add("flashing-border");
+}
